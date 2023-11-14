3a43495f0087961b6d4980c156a74bb2c025f060
Rishabh Bhasin <rishabh.bhasin.33@gmail.com>
2023-11-13 22:41:49 -0800

    Initial command response

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello')
 
 intents = discord.Intents.default()
 intents.message_content = True
